4f3e248f2eee5653f2830f28ffdb19395f0ee6fd
Zephyrroth <126805129+Zephyrroth@users.noreply.github.com>
2023-04-24 15:31:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const sendButton = document.getElementById("send-button");
+
+sendButton.addEventListener("click", function() {
+  alert("Thanks for reaching out! You'll receive a response soon");
+});
